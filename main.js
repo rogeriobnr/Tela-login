9e3decf21437efe10065f9b35dd27c160cc1144a
rogeriobnr <rogeriobnr@gmail.com>
2022-09-08 23:25:46 -0300

    tela-login

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var password = document.getElementById("password");
+    if (password.type === "password") {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  }
